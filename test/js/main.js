e113363aec0d89cb10862c1d33a70629045e35b6
Curious-MX <max536854@gmail.com>
2024-07-31 23:11:24 +0330

    Dynamically created Create Box button in JS.

--- test/js/main.js
+++ test/js/main.js
@@ -1,75 +1,156 @@
-let bodySection;
+document.addEventListener("DOMContentLoaded", () => {
+  let bodySection;
+  let headerSection;
 
-const initializebodySection = () => {
-  if (!bodySection) {
-    bodySection = document.createElement("div");
-    bodySection.classList.add("bodySection");
+  const initializeBodySection = () => {
+    if (!bodySection) {
+      bodySection = document.createElement("div");
+      bodySection.classList.add("bodySection");
 
-    const jobsListWrapper = document.createElement("div");
-    jobsListWrapper.classList.add("jobs-list-wrapper");
-    bodySection.appendChild(jobsListWrapper);
+      const jobsListWrapper = document.createElement("div");
+      jobsListWrapper.classList.add("jobs-list-wrapper");
+      bodySection.appendChild(jobsListWrapper);
 
-    document.body.appendChild(bodySection);
-  }
-  return bodySection.querySelector(".jobs-list-wrapper");
-};
+      document.body.appendChild(bodySection);
+    }
+    return bodySection.querySelector(".jobs-list-wrapper");
+  };
 
-const createBox = () => {
-  const jobsListWrapper = initializebodySection();
+  const initializeHeader = () => {
+    if (!headerSection) {
+      headerSection = document.createElement("div");
+      headerSection.classList.add("header-section");
 
-  // ایجاد عناصر جدید
-  const jobsList = document.createElement("div");
-  jobsList.classList.add("jobs-list");
+      const headerTopBar = document.createElement("div");
+      headerTopBar.classList.add("header-top-bar");
 
-  const jobsListHeading = document.createElement("div");
-  jobsListHeading.classList.add("jobs-list-heading");
+      const topBarBlockLeft = document.createElement("div");
+      topBarBlockLeft.classList.add("top-bar-block", "left");
 
-  const jobsListBody = document.createElement("div");
-  jobsListBody.classList.add("jobs-list-body");
-  jobsListBody.id = "new-jobs";
+      const topBarBlock = document.createElement("div");
+      topBarBlock.classList.add("top-bar-block", "center");
 
-  const li = document.createElement("li");
+      const topBarBlockRight = document.createElement("div");
+      topBarBlockRight.classList.add("top-bar-block", "right");
 
-  const jobBlock = document.createElement("div");
-  jobBlock.classList.add("job-block");
-  jobBlock.id = "new-jobs-test";
+      const img = document.createElement("img");
+      img.classList.add("top-bar-logo");
+      img.src =
+        "https://d2k1ftgv7pobq7.cloudfront.net/meta/u/res/images/trello-header-logos/af7af6ed478d3460709d715d9b3f74a4/trello-logo-white.svg";
 
-  const jobNameBlock = document.createElement("div");
-  jobNameBlock.classList.add("job-name-block");
+      headerTopBar.appendChild(topBarBlockLeft);
+      headerTopBar.appendChild(topBarBlock);
+      topBarBlock.appendChild(img);
+      headerTopBar.appendChild(topBarBlockRight);
+      headerSection.appendChild(headerTopBar);
 
-  const jobName = document.createElement("div");
-  jobName.classList.add("job-name");
+      bodySection.prepend(headerSection);
+    }
+  };
 
-  const jobInfoBlock = document.createElement("div");
-  jobInfoBlock.classList.add("job-info-block");
+  const createBox = () => {
+    const jobsListWrapper = initializeBodySection();
+    initializeHeader();
 
-  const jobDate = document.createElement("div");
-  jobDate.classList.add("job-date");
+    const jobsList = document.createElement("div");
+    jobsList.classList.add("jobs-list");
 
-  const userEmail = document.createElement("div");
-  userEmail.classList.add("user-email");
+    const jobsListHeading = document.createElement("div");
+    jobsListHeading.classList.add("jobs-list-heading");
 
-  const jobsListFooter = document.createElement("div");
-  jobsListFooter.classList.add("job-list-footer");
+    const jobsListBody = document.createElement("div");
+    jobsListBody.classList.add("jobs-list-body");
+    jobsListBody.id = "new-jobs";
 
-  const createElement = document.createElement("button");
-  createElement.classList.add("createElement");
+    const li = document.createElement("li");
 
-  const removeElement = document.createElement("button");
-  removeElement.classList.add("removeElement");
+    const jobBlock = document.createElement("div");
+    jobBlock.classList.add("job-block");
+    jobBlock.id = "new-jobs-test";
 
-  // اضافه کردن عناصر به jobsListWrapper
-  jobsListWrapper.appendChild(jobsList);
-  jobsList.appendChild(jobsListHeading);
-  jobsList.appendChild(jobsListBody);
-  jobsListBody.appendChild(li);
-  li.appendChild(jobBlock);
-  jobBlock.appendChild(jobNameBlock);
-  jobNameBlock.appendChild(jobName);
-  jobBlock.appendChild(jobInfoBlock);
-  jobInfoBlock.appendChild(jobDate);
-  jobInfoBlock.appendChild(userEmail);
-  jobsList.appendChild(jobsListFooter);
-  jobsListFooter.appendChild(createElement);
-  jobsListFooter.appendChild(removeElement);
-};
+    const jobNameBlock = document.createElement("div");
+    jobNameBlock.classList.add("job-name-block");
+
+    const jobName = document.createElement("div");
+    jobName.classList.add("job-name");
+    jobName.textContent = "Job name";
+
+    const jobInfoBlock = document.createElement("div");
+    jobInfoBlock.classList.add("job-info-block");
+
+    const jobDate = document.createElement("div");
+    jobDate.classList.add("job-date");
+    jobDate.textContent = "14 Feb";
+
+    const userEmail = document.createElement("div");
+    userEmail.classList.add("user-email");
+    userEmail.textContent = "max536854@gmail.com";
+
+    const jobsListFooter = document.createElement("div");
+    jobsListFooter.classList.add("job-list-footer");
+
+    const createElementButton = document.createElement("button");
+    createElementButton.classList.add("createElement");
+    createElementButton.textContent = "Create Element";
+    createElementButton.addEventListener("click", createElement);
+
+    const removeElement = document.createElement("button");
+    removeElement.classList.add("removeElement");
+    removeElement.textContent = "Remove Element";
+
+    jobsListWrapper.appendChild(jobsList);
+    jobsList.appendChild(jobsListHeading);
+    jobsList.appendChild(jobsListBody);
+    jobsListBody.appendChild(li);
+    li.appendChild(jobBlock);
+    jobBlock.appendChild(jobNameBlock);
+    jobNameBlock.appendChild(jobName);
+    jobBlock.appendChild(jobInfoBlock);
+    jobInfoBlock.appendChild(jobDate);
+    jobInfoBlock.appendChild(userEmail);
+    jobsList.appendChild(jobsListFooter);
+    jobsListFooter.appendChild(createElementButton);
+    jobsListFooter.appendChild(removeElement);
+  };
+
+  const createElement = () => {
+    const jobsListBody = document.querySelector(".jobs-list-body");
+
+    const li = document.createElement("li");
+
+    const jobBlock = document.createElement("div");
+    jobBlock.classList.add("job-block");
+
+    const jobNameBlock = document.createElement("div");
+    jobNameBlock.classList.add("job-name-block");
+
+    const jobName = document.createElement("div");
+    jobName.classList.add("job-name");
+    jobName.textContent = "New Job";
+
+    const jobInfoBlock = document.createElement("div");
+    jobInfoBlock.classList.add("job-info-block");
+
+    const jobDate = document.createElement("div");
+    jobDate.classList.add("job-date");
+    jobDate.textContent = "14 Feb";
+
+    const userEmail = document.createElement("div");
+    userEmail.classList.add("user-email");
+    userEmail.textContent = "newuser@example.com";
+
+    jobsListBody.appendChild(li);
+    li.appendChild(jobBlock);
+    jobBlock.appendChild(jobNameBlock);
+    jobNameBlock.appendChild(jobName);
+    jobBlock.appendChild(jobInfoBlock);
+    jobInfoBlock.appendChild(jobDate);
+    jobInfoBlock.appendChild(userEmail);
+  };
+
+  const createBoxButton = document.createElement("button");
+  createBoxButton.classList.add("createBox");
+  createBoxButton.textContent = "Create Box";
+  createBoxButton.addEventListener("click", createBox);
+  document.body.appendChild(createBoxButton);
+});
